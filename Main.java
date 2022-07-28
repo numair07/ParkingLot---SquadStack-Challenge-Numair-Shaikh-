bfe879ced12850de79776593b2632dc3d8896ea9
Numair Shahid <67222472+numair07@users.noreply.github.com>
2022-07-28 14:57:19 +0000

    Added Code to read from input file and print it. Also created output file.

--- Main.java
+++ Main.java
@@ -1,5 +1,33 @@
+import java.io.*;
+import java.util.*;
+
 class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+  private FileReader fileReader;
+  private FileWriter fileWriter;
+  public BufferedReader bufferedReader;
+  public BufferedWriter bufferedWriter;
+  public Scanner scanner;
+
+  public Main() throws IOException {
+    fileReader = new FileReader("input.txt");
+    fileWriter = new FileWriter("output.txt");
+    bufferedReader = new BufferedReader(fileReader);
+    bufferedWriter = new BufferedWriter(fileWriter);
+    scanner = new Scanner(System.in);
+  }
+  
+  public static void main(String[] args) throws IOException {
+    Main main = new Main();
+
+    //list to store read lines from input.txt
+    List <String> inputContent = new LinkedList<>();
+
+    //read contents of input.txt
+    String lineReader = "";
+    while ((lineReader = main.bufferedReader.readLine()) != null) {
+      inputContent.add(lineReader);
+    }
+
+    System.out.println(inputContent);
   }
 }
